cff0762d05c89f33d26ebb5a87b89feecd29c33a
trigili <jared@trigili.com>
2026-06-05 11:14:01 -0400

    Split legacy callback builder

--- scripts/gritlib/line_repl_legacy.py
+++ scripts/gritlib/line_repl_legacy.py
@@ -171,6 +171,139 @@ def build_line_legacy_dispatch_callback(
     return dispatch_legacy
 
 
+def _resolve_legacy_search_route_callbacks(
+    use_result_func,
+    bridge_command_builder,
+    start_service_func,
+    stop_service_func,
+    service_rows_func,
+    service_record_func,
+    print_bridge_profile_func,
+    delete_bridge_profile_func,
+    route_service_callbacks,
+    search_callbacks,
+):
+    if use_result_func is None and search_callbacks is not None:
+        use_result_func = search_callbacks["use_line_search_result"]
+    if bridge_command_builder is None:
+        bridge_command_builder = route_service_callbacks["bridge_profile_headless_command"]
+    if start_service_func is None:
+        start_service_func = route_service_callbacks["start_line_service"]
+    if stop_service_func is None:
+        stop_service_func = route_service_callbacks["stop_line_service"]
+    if service_rows_func is None:
+        service_rows_func = lambda _cfg: route_service_callbacks["service_rows"]()
+    if service_record_func is None:
+        service_record_func = route_service_callbacks["service_record"]
+    if print_bridge_profile_func is None:
+        print_bridge_profile_func = route_service_callbacks["print_bridge_profile"]
+    if delete_bridge_profile_func is None:
+        delete_bridge_profile_func = route_service_callbacks["delete_bridge_profile"]
+    return {
+        "use_result_func": use_result_func,
+        "bridge_command_builder": bridge_command_builder,
+        "start_service_func": start_service_func,
+        "stop_service_func": stop_service_func,
+        "service_rows_func": service_rows_func,
+        "service_record_func": service_record_func,
+        "print_bridge_profile_func": print_bridge_profile_func,
+        "delete_bridge_profile_func": delete_bridge_profile_func,
+    }
+
+
+def _resolve_legacy_file_target_callbacks(
+    view_path_func,
+    scoped_target_cfg_func,
+    print_target_summary_func,
+    file_callbacks,
+    target_callbacks,
+):
+    if view_path_func is None:
+        view_path_func = file_callbacks["view_path"]
+    if scoped_target_cfg_func is None:
+        scoped_target_cfg = file_callbacks["scoped_target_cfg"]
+        scoped_target_cfg_func = lambda _cfg, target_id, target_label="": scoped_target_cfg(
+            target_id,
+            target_label=target_label,
+        )
+    if print_target_summary_func is None and target_callbacks is not None:
+        print_target_summary_func = target_callbacks["print_target_summary"]
+    return {
+        "view_path_func": view_path_func,
+        "scoped_target_cfg_func": scoped_target_cfg_func,
+        "print_target_summary_func": print_target_summary_func,
+    }
+
+
+def _resolve_legacy_action_callbacks(
+    actions_func,
+    run_workbench_action_func,
+    run_target_workflow_func,
+    action_state_text_func,
+    action_callbacks,
+):
+    if action_callbacks is not None:
+        if actions_func is None:
+            actions_func = lambda _cfg: action_callbacks["workbench_actions"]()
+        if run_workbench_action_func is None:
+            run_workbench_action_func = action_callbacks["run_workbench_action"]
+        if run_target_workflow_func is None:
+            run_target_workflow_func = action_callbacks["run_target_workflow"]
+        if action_state_text_func is None:
+            action_state_text_func = action_callbacks["action_state_text"]
+    return {
+        "actions_func": actions_func,
+        "run_workbench_action_func": run_workbench_action_func,
+        "run_target_workflow_func": run_target_workflow_func,
+        "action_state_text_func": action_state_text_func,
+    }
+
+
+def _build_resolved_line_legacy_dispatch(
+    cfg,
+    input_func,
+    clear_results_func,
+    sleep_func,
+    append_event_fn,
+    print_staged_func,
+    snapshot_func,
+    route_service_callbacks,
+    file_callbacks,
+    queue_callbacks,
+    route_resolved,
+    target_resolved,
+    action_resolved,
+):
+    return build_line_legacy_dispatch_callback(
+        cfg,
+        input_func=input_func,
+        use_result_func=route_resolved["use_result_func"],
+        clear_results_func=clear_results_func,
+        start_service_func=route_resolved["start_service_func"],
+        stop_service_func=route_resolved["stop_service_func"],
+        line_start_service_func=route_service_callbacks["start_line_service"],
+        line_stop_service_func=route_service_callbacks["stop_line_service"],
+        service_rows_func=route_resolved["service_rows_func"],
+        service_record_func=route_resolved["service_record_func"],
+        sleep_func=sleep_func,
+        append_event_fn=append_event_fn,
+        print_staged_func=print_staged_func,
+        snapshot_func=snapshot_func,
+        view_path_func=target_resolved["view_path_func"],
+        stage_binary_func=file_callbacks["stage_binary"],
+        actions_func=action_resolved["actions_func"],
+        run_workbench_action_func=action_resolved["run_workbench_action_func"],
+        run_target_workflow_func=action_resolved["run_target_workflow_func"],
+        scoped_target_cfg_func=target_resolved["scoped_target_cfg_func"],
+        print_target_summary_func=target_resolved["print_target_summary_func"],
+        bridge_command_builder=route_resolved["bridge_command_builder"],
+        print_bridge_profile_func=route_resolved["print_bridge_profile_func"],
+        delete_bridge_profile_func=route_resolved["delete_bridge_profile_func"],
+        action_state_text_func=action_resolved["action_state_text_func"],
+        print_queue_func=queue_callbacks["print_line_command_queue_view"],
+    )
+
+
 def build_line_legacy_callbacks(
     cfg,
     *,
@@ -202,67 +335,44 @@ def build_line_legacy_callbacks(
     search_callbacks=None,
     action_callbacks=None,
 ):
-    if use_result_func is None and search_callbacks is not None:
-        use_result_func = search_callbacks["use_line_search_result"]
-    if bridge_command_builder is None:
-        bridge_command_builder = route_service_callbacks["bridge_profile_headless_command"]
-    if start_service_func is None:
-        start_service_func = route_service_callbacks["start_line_service"]
-    if stop_service_func is None:
-        stop_service_func = route_service_callbacks["stop_line_service"]
-    if service_rows_func is None:
-        service_rows_func = lambda _cfg: route_service_callbacks["service_rows"]()
-    if service_record_func is None:
-        service_record_func = route_service_callbacks["service_record"]
-    if print_bridge_profile_func is None:
-        print_bridge_profile_func = route_service_callbacks["print_bridge_profile"]
-    if delete_bridge_profile_func is None:
-        delete_bridge_profile_func = route_service_callbacks["delete_bridge_profile"]
-    if view_path_func is None:
-        view_path_func = file_callbacks["view_path"]
-    if scoped_target_cfg_func is None:
-        scoped_target_cfg = file_callbacks["scoped_target_cfg"]
-        scoped_target_cfg_func = lambda _cfg, target_id, target_label="": scoped_target_cfg(
-            target_id,
-            target_label=target_label,
-        )
-    if print_target_summary_func is None and target_callbacks is not None:
-        print_target_summary_func = target_callbacks["print_target_summary"]
-    if action_callbacks is not None:
-        if actions_func is None:
-            actions_func = lambda _cfg: action_callbacks["workbench_actions"]()
-        if run_workbench_action_func is None:
-            run_workbench_action_func = action_callbacks["run_workbench_action"]
-        if run_target_workflow_func is None:
-            run_target_workflow_func = action_callbacks["run_target_workflow"]
-        if action_state_text_func is None:
-            action_state_text_func = action_callbacks["action_state_text"]
-    dispatch_legacy = build_line_legacy_dispatch_callback(
-        cfg,
-        input_func=input_func,
-        use_result_func=use_result_func,
-        clear_results_func=clear_results_func,
-        start_service_func=start_service_func,
-        stop_service_func=stop_service_func,
-        line_start_service_func=route_service_callbacks["start_line_service"],
-        line_stop_service_func=route_service_callbacks["stop_line_service"],
-        service_rows_func=service_rows_func,
-        service_record_func=service_record_func,
-        sleep_func=sleep_func,
-        append_event_fn=append_event_fn,
-        print_staged_func=print_staged_func,
-        snapshot_func=snapshot_func,
-        view_path_func=view_path_func,
-        stage_binary_func=file_callbacks["stage_binary"],
-        actions_func=actions_func,
-        run_workbench_action_func=run_workbench_action_func,
-        run_target_workflow_func=run_target_workflow_func,
-        scoped_target_cfg_func=scoped_target_cfg_func,
-        print_target_summary_func=print_target_summary_func,
-        bridge_command_builder=bridge_command_builder,
-        print_bridge_profile_func=print_bridge_profile_func,
-        delete_bridge_profile_func=delete_bridge_profile_func,
-        action_state_text_func=action_state_text_func,
-        print_queue_func=queue_callbacks["print_line_command_queue_view"],
+    route_resolved = _resolve_legacy_search_route_callbacks(
+        use_result_func,
+        bridge_command_builder,
+        start_service_func,
+        stop_service_func,
+        service_rows_func,
+        service_record_func,
+        print_bridge_profile_func,
+        delete_bridge_profile_func,
+        route_service_callbacks,
+        search_callbacks,
     )
-    return {"dispatch_line_legacy": dispatch_legacy}
+    target_resolved = _resolve_legacy_file_target_callbacks(
+        view_path_func,
+        scoped_target_cfg_func,
+        print_target_summary_func,
+        file_callbacks,
+        target_callbacks,
+    )
+    action_resolved = _resolve_legacy_action_callbacks(
+        actions_func,
+        run_workbench_action_func,
+        run_target_workflow_func,
+        action_state_text_func,
+        action_callbacks,
+    )
+    return {"dispatch_line_legacy": _build_resolved_line_legacy_dispatch(
+        cfg,
+        input_func,
+        clear_results_func,
+        sleep_func,
+        append_event_fn,
+        print_staged_func,
+        snapshot_func,
+        route_service_callbacks,
+        file_callbacks,
+        queue_callbacks,
+        route_resolved,
+        target_resolved,
+        action_resolved,
+    )}
